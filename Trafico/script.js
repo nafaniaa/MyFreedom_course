2b2e1579b48b11264c4835ec86802383210e6939
Tatyana-Gurinovich <tgurinovich2005@gmail.com>
2021-02-24 18:16:34 +0300

    .

---
+++ Trafico/script.js
@@ -0,0 +1,12 @@
+document.querySelectorAll('.faq__head').forEach((item) =>
+    item.addEventListener('click', () => {
+        const parent = item.parentNode;
+        if (parent.classList.contains('faq__item--active')) {
+            parent.classList.remove('faq__item--active');
+
+        } else {
+            document.querySelectorAll('.faq__item').forEach((child) => child.classList.remove('faq__item--active'));        
+            parent.classList.add('faq__item--active');
+        }       
+    })
+)
