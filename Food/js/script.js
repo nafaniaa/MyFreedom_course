6cba98fc062f5320e887db4bf6842a733c63e459
Tatyana-Gurinovich <tgurinovich2005@gmail.com>
2021-03-29 18:51:23 +0300

    Всплывающее окно

--- Food/js/script.js
+++ Food/js/script.js
@@ -87,5 +87,53 @@ window.addEventListener('DOMContentLoaded', function () {
     }
 
     setClock('.timer', deadline)
+
+    const modalTrigger = document.querySelectorAll('[data-modal]'),
+    modal = document.querySelector('.modal'),
+    modalCloseBtn = document.querySelector('[data-close]')
+
+    function closeModal(){
+        modal.classList.add('hide')
+        modal.classList.remove('show')
+        document.body.style.overflow=''
+    }
+
+    function openModal(){
+        modal.classList.add('show')
+        modal.classList.remove('hide')
+        document.body.style.overflow='hidden'
+        clearTimeout(modalTimerId)
+    }
+
+    modalTrigger.forEach(btn =>{
+        btn.addEventListener('click',openModal)
+    })
+
+    modalCloseBtn.addEventListener('click', closeModal)
+
+    modal.addEventListener('click',(e)=>{
+        if(e.target === modal){
+            closeModal
+        }
+    })
+
+    document.addEventListener('keydown',(e)=>{
+        if(e.code ==='Escape' && modal.classList.contains('show')){
+            closeModal()
+        }
+    })
+
+    const modalTimerId = setTimeout(openModal,3000)
+
+    function showModalByScroll(){
+        if(window.pageYOffset
+             + document.documentElement.clientHeight 
+              >= document.documentElement.scrollHeight){
+                  openModal()
+                  window.removeEventListener('scroll', showModalByScroll)
+              }
+    }
+
+    window.addEventListener('scroll', showModalByScroll)
 });
     //когда контент HTML подгрузился
