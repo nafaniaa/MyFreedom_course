493b175dc14d3028b96e8ca41bc0d6ffac025883
Tatyana-Gurinovich <tgurinovich2005@gmail.com>
2021-04-26 18:51:08 +0300

    Слайдер

---
+++ PROJECT_Showroom/js/tanya.js
@@ -0,0 +1,58 @@
+let position = 0;
+const slidesToShow = 3;
+const slidesToScroll = 2;
+const container = document.querySelector('.slider-container');
+const track = document.querySelector('.slider-track');
+const items = document.querySelectorAll('.slider-item');
+const btnPrev = document.querySelector('.btn-prev');
+const btnNext = document.querySelector('.btn-next');
+const itemsCount = items.length;
+const itemWidth = container.clientWidth / slidesToShow;
+const movePosition = slidesToScroll * itemWidth;
+
+
+items.forEach((item) => {
+    item.style.minWidth = `${itemWidth}px`;
+});
+
+
+// btnPrev.addEventListener('click', () => {
+//     const itemLeft = Math.abs(position) / itemWidth;
+//     // position += itemLeft >= slidesToScroll ? movePosition : itemLeft * itemWidth;
+
+//     if (itemLeft >= slidesToScroll) {
+//         position = position + movePosition;
+//     } else {
+//         position = position + itemLeft * itemWidth;
+//     }
+
+//     setPosition();
+//     checkBtns();
+// });
+
+// btnNext.addEventListener('click', () => {
+//     const itemLeft = itemsCount - (Math.abs(position) + slidesToShow * itemWidth) / itemWidth;
+//     // position -= itemLeft >= slidesToScroll ? movePosition : itemLeft * itemWidth;
+
+//     if (itemLeft >= slidesToScroll) {
+//         position -= movePosition;
+//     } else {
+//         position -= itemLeft * itemWidth;
+//     }
+
+//     setPosition();
+//     checkBtns();
+// });
+
+
+const setPosition = () => {
+    track.style.transform = `translateX(-200px)`;
+};
+
+// const checkBtns = () => {
+//     btnPrev.disabled = position === 0;
+//     btnNext.disabled = position <= -(itemsCount - slidesToShow) * itemWidth;
+// };
+// checkBtns();
+
+const time = setInterval(setPosition, 2000)
